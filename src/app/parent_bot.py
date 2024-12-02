8ffe951d8f435b3d7c5a5a0e550eb7ac4b41b025
Solene <solene.moine@depinfonancy.net>
2024-12-02 08:18:29 +0100

    created a txt file with all the ids and names of datasets from data.public.lu. parent_bot now can recognize intent about id and check if dataset has a csv.

--- src/app/parent_bot.py
+++ src/app/parent_bot.py
@@ -3,10 +3,18 @@
 # sys.path.append("/Path/to/directory/bot-framework") # Replace with your directory path
 
 import logging
+import requests
+import json
 
 from besser.bot.core.bot import Bot
 from besser.bot.core.session import Session
 from besser.bot.platforms.websocket import WEBSOCKET_PORT
+from besser.bot.nlp.intent_classifier.intent_classifier_prediction import IntentClassifierPrediction
+
+from besser.bot.library.entity.base_entities import any_entity
+
+#from besser.bot.nlp.llm.llm_openai_api import LLMOpenAI
+#from besser.bot.nlp.intent_classifier.intent_classifier_configuration import LLMIntentClassifierConfiguration
 
 # Configure the logging module
 logging.basicConfig(level=logging.INFO, format='{levelname} - {asctime}: {message}', style='{')
@@ -18,55 +26,114 @@ websocket_platform = parent_bot.use_websocket_platform(use_ui=False)
 
 # STATES
 
-initial_state = parent_bot.new_state('initial_state', initial=True)
-hello_state = parent_bot.new_state('hello_state')
-good_state = parent_bot.new_state('good_state')
-bad_state = parent_bot.new_state('bad_state')
+initial_state = parent_bot.new_state('initial_parent_botstate', initial=True)
+name_state = parent_bot.new_state('name_state')
+id_state = parent_bot.new_state('id_state')
+update_state = parent_bot.new_state('update_state')
+
+# ENTITIES
+with open("src/app/datasets_id_names.txt", "r") as file:
+    name_id_dict = json.load(file)
+id_entries = {key: [] for key in name_id_dict.keys()}
+
+id_entity = parent_bot.new_entity(
+    name='id_entity',
+    description='The ID of a dataset, a long string containing numbers and a few alphabetical characters',
+    entries=id_entries
+)
+
+name_entity = parent_bot.new_entity(
+    name='name_entity',
+    description='The name of a dataset, a string of alphabetical characters',
+    entries={
+    'name1': [],
+    'name2': [],
+    }
+)
 
 # INTENTS
 
-hello_intent = parent_bot.new_intent('hello_intent', [
-    'hello',
-    'hi',
+id_intent = parent_bot.new_intent('id_intent', [
+    'What is the dataset of id ID?',
+    'id dataset ID',
+    'dataset id ID',
+])
+id_intent.parameter('id1', 'ID', id_entity)
+
+name_intent = parent_bot.new_intent('name_intent', [
+    'What dataset is called NAME?',
+    'name dataset NAME',
+    'dataset name NAME',
+])
+name_intent.parameter('name1', 'NAME', name_entity)
+
+update_intent = parent_bot.new_intent('update_intent', [
+    'update',
 ])
 
-good_intent = parent_bot.new_intent('good_intent', [
-    'good',
-    'fine',
-])
+# OTHER FUNCTIONS
 
-bad_intent = parent_bot.new_intent('bad_intent', [
-    'bad',
-    'awful',
-])
+def updateDatabase():
+    dict = {} #dictionary to link name and id of every dataset
+    url = "https://data.public.lu/api/1/datasets/"
+    while(url):
+        response = requests.get(url).json()
+        for dataset in response['data']:
+            dict[dataset['id']] = dataset['title'] #the ids are unique so they are the keys
+        url = response['next_page']
+    with open("src/app/datasets_id_names.txt", "w") as file:
+        json.dump(dict, file, indent=4)
 
 
 # STATES BODIES' DEFINITION + TRANSITIONS
 
-
-initial_state.when_intent_matched_go_to(hello_intent, hello_state)
-
-
-def hello_body(session: Session):
-    session.reply('Hi! How are you?')
+def confused_body(session: Session):
+    session.reply("I feel like you are not making any sense.")
 
 
-hello_state.set_body(hello_body)
-hello_state.when_intent_matched_go_to(good_intent, good_state)
-hello_state.when_intent_matched_go_to(bad_intent, bad_state)
+def initial_body(session: Session):
+    session.reply("Hello! Give me a dataset ID or name and I will retrieve it!")
+
+initial_state.set_body(initial_body)
+initial_state.when_intent_matched_go_to(id_intent, id_state)
+initial_state.when_intent_matched_go_to(name_intent, name_state)
+initial_state.when_intent_matched_go_to(update_intent, update_state)
+initial_state.set_fallback_body(confused_body)
 
 
-def good_body(session: Session):
-    session.reply('I am glad to hear that!')
+def name_body(session: Session):
+    session.reply('I cannot give you any information about this yet. But my next update surely will fix this !')
+
+name_state.set_body(name_body)
+name_state.go_to(initial_state)
 
 
-good_state.set_body(good_body)
-good_state.go_to(initial_state)
+def id_body(session: Session):
+    predicted_intent: IntentClassifierPrediction = session.predicted_intent
+    id = predicted_intent.get_parameter('id1')
+    if id.value is None:
+        session.reply("Sorry, it seems this id does not exist. If you think this is a mistake you can ask me to update my database.")
+    else:
+        url = "https://data.public.lu/api/1/datasets/" + str(id.value) + "/"
+        response = requests.get(url).json()
+        print(json.dumps(response,indent=4))
+        format = ""
+        for resource in response['resources']:
+                if resource['format'] == "csv":
+                    format = resource['format']
+                    break
+        if format == "csv":
+            session.reply(f"It seems the {id.value} dataset has a csv.")
+        else:
+            session.reply(f"It seems the {id.value} dataset does not have any csv.")
 
+id_state.set_body(id_body)
+id_state.go_to(initial_state)
 
-def bad_body(session: Session):
-    session.reply('I am sorry to hear that...')
+def update_body(session: Session):
+    session.reply('I will start updating my database and tell you when I am done. Please wait, it will take a little while.')
+    updateDatabase()
+    session.reply('The update is done !')
 
-
-bad_state.set_body(bad_body)
-bad_state.go_to(initial_state)
+update_state.set_body(update_body)
+update_state.go_to(initial_state)
